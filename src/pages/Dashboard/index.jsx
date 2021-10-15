948beba397b4811d770fc1d1585d52725ac4e50f
Ricardo <ricardonegocios700@gmail.com>
2021-10-15 16:04:33 -0300

    created dashboard page, with preview of buttons

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,3 +1,26 @@
+import dashboardImg from "../../assets/img/clip-uniting-the-world.png";
+import { DasboardCSS } from "./style";
+import Button from "../../components/Button/index.jsx";
 export const Dashboard = () => {
-  return <h1>Novo Dashboard</h1>;
+  const handleClickGroups = () => {
+    console.log("handleClickGroups");
+  };
+  const handleClickHabits = () => {
+    console.log("handleClickHabits");
+  };
+  const handleClickUpdUser = () => {
+    console.log("handleClickUpdUser");
+  };
+  return (
+    <DasboardCSS>
+      <Button handleClick={handleClickGroups} type="onClick" title="Grupos" />
+      <Button handleClick={handleClickHabits} type="onClick" title="Hábitos" />
+      <Button
+        handleClick={handleClickUpdUser}
+        type="onClick"
+        title="Atualizar Cadastro"
+      />
+      <img src={dashboardImg} alt="imagem dashboard" />
+    </DasboardCSS>
+  );
 };
---
+++ src/pages/Dashboard/style.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const DasboardCSS = styled.div`
+  img {
+    width: 300px;
+  }
+`;
