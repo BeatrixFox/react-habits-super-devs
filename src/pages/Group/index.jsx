85c6a2c19f8d6ed290a94aea36c3218ba962a2eb
Ricardo <ricardonegocios700@gmail.com>
2021-10-15 21:05:13 -0300

    feat(group): created basic page of selected group

--- src/pages/Group/index.jsx
+++ src/pages/Group/index.jsx
@@ -2,10 +2,10 @@ import { Container } from "./style";
 
 export const Group = () => {
   return (
-    <>
-      <p>Componet metas</p>
-      <p>Componet atividades</p>
-      <p>Componet membros</p>
-    </>
+    <Container>
+      <p>Aqui vai o componet metas</p>
+      <p>Aqui vai o componet atividades</p>
+      <p>Aqui vai o componet membros</p>
+    </Container>
   );
 };
