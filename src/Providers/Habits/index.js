c862173cd3dfe36a453393425ce131425a98ac9e
Ricardo <ricardonegocios700@gmail.com>
2021-10-16 15:50:36 -0300

    feat(provider_habits): add, delete and update functions created

--- src/Providers/Habits/index.js
+++ src/Providers/Habits/index.js
@@ -3,7 +3,7 @@ import api from "../../services/api";
 
 export const HabitsContext = createContext([]);
 
-//TODO mudar para um provider
+//TODO mudar para um stateProvider
 const token =
   "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjM0NjAyMDczLCJqdGkiOiJiZGU1ODJjMDFkNjk0MmE4YmNhMjlhZDY2ZDNmMDNmNiIsInVzZXJfaWQiOjF9.LoM4YMt6Ky-xUVEI47nChgVerjnCmCU2G4hrjug5peQ";
 
@@ -13,6 +13,7 @@ const config = {
 
 export const HabitsProvider = ({ children }) => {
   const [habits, setHabits] = useState([]);
+
   const getHabits = () => {
     api
       .get("habits/personal/", config)
@@ -20,15 +21,37 @@ export const HabitsProvider = ({ children }) => {
       .catch((error) => console.log("Erro: ", error));
   };
   useEffect(() => {
-    habits !== [] && getHabits();
+    /*habits !== [] &&*/ getHabits();
   }, []);
 
-  const addAHabit = () => {};
-  const removeAHabit = () => {};
+  const addAHabit = (date) => {
+    api
+      .post("habits/", date, config)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => console.log(error));
+  };
+  const removeAHabit = (id) => {
+    api
+      .delete(`habits/${id}/`, config)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => console.log(error));
+  };
+  const updateAHabit = (date) => {
+    api
+      .patch(`habits/${date.id}/`, date, config)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => console.log(error));
+  };
 
   return (
     <HabitsContext.Provider
-      value={{ habits, setHabits, addAHabit, removeAHabit }}
+      value={{ habits, setHabits, addAHabit, removeAHabit, updateAHabit }}
     >
       {children}
     </HabitsContext.Provider>
