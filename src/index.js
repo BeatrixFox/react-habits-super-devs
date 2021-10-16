5fcf7f4fa2b7684fecb689dadfd3a7a1b8e4a91d
Ricardo <ricardonegocios700@gmail.com>
2021-10-16 17:41:03 -0300

    feat(provider_groups): Fixed error in Provider, missing a /

--- src/index.js
+++ src/index.js
@@ -3,16 +3,15 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
-import Providers from './Providers';
+import Providers from "./Providers/index";
 
 ReactDOM.render(
   <React.StrictMode>
-  <Providers>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Providers>
-
+    <Providers>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Providers>
   </React.StrictMode>,
   document.getElementById("root")
 );
