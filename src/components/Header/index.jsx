0e256c7f618e509e49f6eb565c2f8be42e5fe7fa
Ricardo <ricardonegocios700@gmail.com>
2021-10-18 12:18:33 -0300

    feat(titleName): added functionality to display title in bar

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,37 @@
 import AssignmentTurnedInOutlinedIcon from "@mui/icons-material/AssignmentTurnedInOutlined";
 import LogoutOutlinedIcon from "@mui/icons-material/LogoutOutlined";
+import { useLocation } from "react-router-dom";
 
 import { HeaderDiv, Link, Logout } from "./style";
 
-function Header({ titleMessage }) {
+function Header() {
+  const locate = useLocation();
+  console.log(locate.pathname);
+
+  const titleMessage = () => {
+    switch (locate.pathname) {
+      case "/":
+        return "Hábitos SuperDev's";
+      case "/habits":
+        return "Meus Hábitos";
+      case "/login":
+        return "Login";
+      case "/cadastro":
+        return "Cadastro";
+      case "/dashboard":
+        return "Painel do SuperDev";
+      case "/my_groups":
+        return "Meus Grupos";
+      case "/groups":
+        return "Grupos";
+      case "/group":
+        //TODO aqui precisamos chamar o nome do grupo
+        return "Grupo Selecionado";
+
+      default:
+        break;
+    }
+  };
   return (
     <HeaderDiv>
       <a href="/">
@@ -14,7 +42,7 @@ function Header({ titleMessage }) {
       <Link> Hábitos </Link>
       <Link> Grupos </Link>
 
-      <h1>{titleMessage}</h1>
+      <h1>{titleMessage()}</h1>
 
       <Logout>
         <LogoutOutlinedIcon />
