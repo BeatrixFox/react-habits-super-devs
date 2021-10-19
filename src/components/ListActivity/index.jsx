9d3169afb6429cad0e82f698b21e66844632d4d4
Ricardo <ricardonegocios700@gmail.com>
2021-10-19 13:26:32 -0300

    fix(ativities_card): solving conflit

--- src/components/ListActivity/index.jsx
+++ src/components/ListActivity/index.jsx
@@ -1,13 +1,6 @@
 import { Container, Div } from "./style";
 import AtivityCard from "../ActivityCard/index";
 
-<<<<<<< HEAD
-const ListActivity = ({ oneGroup }) => {
-  return (
-    <Container>
-      {oneGroup.activities.map((item) => (
-        <p>Título: {item.title}</p>
-=======
 import { ActivitiesHabitsApiContext } from "../../Providers/activitiesHabitsApi";
 
 import { useContext } from "react";
@@ -21,7 +14,6 @@ export const ListActivity = () => {
         <li key={activity.id}>
           <p>Cardlist {activity}</p>
         </li>
->>>>>>> develop
       ))}
     </Container>
   );
