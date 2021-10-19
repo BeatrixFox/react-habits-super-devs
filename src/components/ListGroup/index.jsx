40b36fdedcf9547975db52b2bd0bc7a9e461c583
Sthefany Gomes <fany@ubuntu.local>
2021-10-19 04:22:05 -0300

    fix: Groups path and ListGroup

--- src/components/ListGroup/index.jsx
+++ src/components/ListGroup/index.jsx
@@ -1,29 +1,39 @@
 import { useContext } from "react";
+
 import { Container } from "./style";
+
 import { GroupsContext } from "../../Providers/Groups/index";
+
 import GroupsCard from "../../components/GroupsCard/index";
+
 import { useLocation } from "react-router-dom";
 
 export const ListGroup = () => {
   const location = useLocation();
-  const locationPath = location.pathname;
-  const { groups, myGroups } = useContext(GroupsContext);
-  const object = () => {
-    if (locationPath === "/my_groups") {
-      return myGroups;
-    }
-    if (locationPath === "/groups") {
-      return groups;
-    }
-  };
 
-  return (
-    <Container>
-      {object().map((group) => (
-        <GroupsCard item={group} />
-      ))}
-    </Container>
-  );
+  const locationPath = location.pathname;
+
+  const { groups, myGroups } = useContext(GroupsContext);
+
+  if (locationPath === "/my_groups") {
+    return (
+      <Container>
+        {myGroups.map((group) => (
+          <GroupsCard item={group} />
+        ))}
+      </Container>
+    );
+  }
+
+  if (locationPath === "/groups") {
+    return (
+      <Container>
+        {groups.map((group) => (
+          <GroupsCard item={group} />
+        ))}
+      </Container>
+    );
+  }
 };
 
 export default ListGroup;
