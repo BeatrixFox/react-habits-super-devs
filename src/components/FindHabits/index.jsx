a7e6321623aa5b3edf40983de3aa762a207e0031
Ricardo <ricardonegocios700@gmail.com>
2021-10-19 15:43:07 -0300

    (feature/find_habits_modal): break to merge with develop

--- src/components/FindHabits/index.jsx
+++ src/components/FindHabits/index.jsx
@@ -1,9 +1,32 @@
 import Button from "../Button/index";
+import { Modal, TextField, Box } from "@material-ui/core";
 import { useContext, useState } from "react";
 import { HabitsContext } from "../../Providers/Habits";
 import { Container } from "./styles";
 
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: {
+    xs: 300,
+    sm: 500,
+    xl: 600,
+  },
+
+  bgcolor: "background.paper",
+  border: "2px solid #007aff",
+  borderRadius: "10px",
+  boxShadow: 24,
+  p: 4,
+};
+
 const FindHabits = () => {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   const [text, setText] = useState("");
   const { getOneHabit, oneHabit } = useContext(HabitsContext);
 
@@ -13,17 +36,31 @@ const FindHabits = () => {
 
   return (
     <Container>
-      <input
-        type="text"
-        value={text}
-        onChange={(event) => setText(event.target.value)}
-      />
-      <Button
-        title="Procurar"
-        type="click"
-        handlerClick={() => handlerClickFinding(text)}
-      />
-      <p>CardHabit {oneHabit}</p>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <h1>Procurar Hábitos</h1>
+          <TextField
+            fullWidth
+            label="Novo username"
+            margin="normal"
+            variant="filled"
+            size="small"
+            color="primary"
+            onChange={(event) => setText(event.target.value)}
+          />
+          <Button
+            title="Procurar"
+            type="click"
+            handlerClick={() => handlerClickFinding(text)}
+          />
+          <p>CardHabit {oneHabit}</p>
+        </Box>
+      </Modal>
     </Container>
   );
 };
