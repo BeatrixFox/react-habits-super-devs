5ace7f37da32c16c74bf313337b338b8aacd89b2
Sthefany Gomes <fany@ubuntu.local>
2021-10-20 17:05:27 -0300

    feat: add search group modal

--- src/components/FindGroup/index.jsx
+++ src/components/FindGroup/index.jsx
@@ -2,28 +2,83 @@ import Button from "../Button";
 import { useContext, useState } from "react";
 import { Container } from "./styles";
 import { GroupsContext } from "../../Providers/Groups";
+import { Modal, TextField, Box } from "@material-ui/core";
+import GroupsCard from "../GroupsCard";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: {
+    xs: 300,
+    sm: 500,
+    xl: 600,
+  },
+
+  bgcolor: "background.paper",
+  border: "2px solid #007aff",
+  borderRadius: "10px",
+  boxShadow: 24,
+  p: 4,
+};
 
 const FindGroup = () => {
   const [text, setText] = useState("");
-  const { getOneGroups, groups } = useContext(GroupsContext);
+  const { getOneGroup, groups, oneGroup } = useContext(GroupsContext);
+
+  //////////////////////////////////////////////////////modal
+
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  //////////////////////////////////////////////////////endModal
 
   const handlerClickFinding = (text) => {
     const oneTime = groups.filter((group) => group.name === text);
-    getOneGroups(oneTime.id);
+    // console.log(oneTime.id);
+    getOneGroup(oneTime.id);
   };
 
   return (
     <Container>
-      <input
-        type="text"
-        value={text}
-        onChange={(event) => setText(event.target.value)}
-      />
-      <Button
-        title="Procurar"
-        type="click"
-        handleClick={() => handlerClickFinding(text)}
-      />
+      <Button title="Procurar Grupo" handleClick={handleOpen} />
+
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <h1>Procurar grupo</h1>
+          <TextField
+            fullWidth
+            label="Digite o nome do grupo"
+            margin="normal"
+            variant="filled"
+            size="small"
+            color="primary"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+
+          <Button
+            title="Procurar"
+            type="click"
+            handleClick={() => handlerClickFinding(text)}
+          />
+
+          <p>
+            {oneGroup.map((group) => (
+              <li key={group.id}>
+                <GroupsCard item={group} />
+              </li>
+            ))}
+          </p>
+        </Box>
+      </Modal>
     </Container>
   );
 };
