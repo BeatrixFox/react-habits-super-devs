4aa09cb1a65a0da75197d6c27389f4b2838e6c37
Sthefany Gomes <fany@ubuntu.local>
2021-10-20 09:29:21 -0300

    feat: add delete activity

--- src/components/ActivityCard/index.jsx
+++ src/components/ActivityCard/index.jsx
@@ -1,12 +1,32 @@
 import { Div, Container } from "./styles";
+import { ActivitiesHabitsApiContext } from "../../Providers/activitiesHabitsApi";
+import { useContext } from "react";
+import Button from "../Button";
 
 const AtivityCard = ({ item }) => {
+  const { deleteActivity } = useContext(ActivitiesHabitsApiContext);
+
+  const handleClickDelete = (activityId) => {
+    deleteActivity(activityId);
+    console.log(activityId);
+  };
+
   return (
     <Container key={item.id}>
       <span>{item.title}</span>
       <Div>
         <span>Título: {item.title}</span>
         <span>Prazo: {item.realization_time}</span>
+        <Button
+          type="onClick"
+          title="Atualizar"
+          handleClick={() => handleClickDelete(item.id)}
+        />
+        <Button
+          type="onClick"
+          title="Deletar"
+          handleClick={() => handleClickDelete(item.id)}
+        />
       </Div>
     </Container>
   );
